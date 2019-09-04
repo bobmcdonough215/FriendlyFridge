1fc211f613cb17c5257e85e81260a94bc3d35d8c
Josh Ellsworth <joshuaellsworth1@gmail.com>
2019-09-04 12:31:04 -0400

    More merge fixes

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-<<<<<<< HEAD
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-=======
-// import "./index.css";
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
->>>>>>> 44dbce004fc56f27b040d703c569d4b0d05a8a6d
