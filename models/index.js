949f622ee7927a93c6943c7fb27bdf2b6724d1d5
Josh Ellsworth <joshuaellsworth1@gmail.com>
2019-09-04 12:30:07 -0400

    Fixed merge from master

--- models/index.js
+++ models/index.js
@@ -1,8 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
-    Food: require("./food")
-  };
-=======
     Food: require("./food"),
     User: require("./user"),
     // List: require("./list"),
@@ -10,4 +6,3 @@ module.exports = {
 
   };
   
->>>>>>> 44dbce004fc56f27b040d703c569d4b0d05a8a6d
