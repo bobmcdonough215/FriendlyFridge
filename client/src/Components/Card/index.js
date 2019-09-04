256da674e1ef3edd48101fd49b43df806089e6a8
Ying Huang <huangying270@gmail.com>
2019-09-04 14:10:30 -0400

    added card, delete btn, jumbotron components

---
+++ client/src/Components/Card/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Card({ children }) {
+    return (
+        <div className="card">
+            <div className="card-body">
+                {children}
+            </div>
+        </div>
+
+    );
+}
+
+export default Card;
