729b99b80ee3533b8e6f56ed4db467ebdca7f378
Josh Ellsworth <joshuaellsworth1@gmail.com>
2019-09-04 14:02:00 -0400

    nav bar buttons link to other pages

---
+++ client/src/pages/myfridge.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class myfridge extends Component {
+    render() {
+        return (
+            <div className="container">
+                My Fridge Page
+            </div>
+        )
+    }
+}
+
+export default myfridge;
